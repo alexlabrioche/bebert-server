34e5378ffff5a24b6ae29e2f5c41aeb472ee1889
Alex Baker <alexbakerdeveloper@gmail.com>
2019-11-10 13:58:04 +0100

    starting pubSub real time

---
+++ src/api/User/GetNearbyDrivers/GetNearbyDrivers.resolvers.ts
@@ -0,0 +1,37 @@
+import { Resolvers } from 'src/types/resolvers';
+import authResolver from 'src/utils/authResolver';
+import { GetNearbyDriversResponse } from 'src/types/graph';
+import User from 'src/entity/User';
+import { getRepository, Between } from 'typeorm';
+
+const resolvers: Resolvers = {
+  Query: {
+    GetNearbyDrivers: authResolver(
+      async (_, __, context): Promise<GetNearbyDriversResponse> => {
+        const logguedUser: User = context.req.user;
+        const { lastLat, lastLng } = logguedUser;
+        try {
+          // call table User without active repository initiated in typeORM entities
+          // Between filter must be called with getRepository
+          const drivers: User[] = await getRepository(User).find({
+            isDriving: true,
+            lastLat: Between(lastLat - 0.05, lastLat + 0.05),
+            lastLng: Between(lastLng - 0.05, lastLng + 0.05),
+          });
+          return {
+            ok: false,
+            error: 'pas autorisé',
+            drivers,
+          };
+        } catch (error) {
+          return {
+            ok: false,
+            error: error.message,
+            drivers: null,
+          };
+        }
+      },
+    ),
+  },
+};
+export default resolvers;
